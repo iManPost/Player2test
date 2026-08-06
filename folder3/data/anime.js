b78e30d2769e2cc742c1f9fdd7aa837af7bb8087
iManPost <iveenvon@gmail.com>
2026-08-06 10:22:48 +0700

    Update anime.js

--- folder3/data/anime.js
+++ folder3/data/anime.js
@@ -1,6 +1,11 @@
  export const anime = [
 
-
+       {
+    name: ' ห้องเรียนลอบสังหาร เดอะ มูฟวี่',
+    group: 'Anime',
+    logo: 'https://madaboutmoviez.com/wp-content/uploads/Assassination-Classroom-the-Movie-Our-Time-%E0%B8%AB%E0%B9%89%E0%B8%AD%E0%B8%87%E0%B9%80%E0%B8%A3%E0%B8%B5%E0%B8%A2%E0%B8%99%E0%B8%A5%E0%B8%AD%E0%B8%9A%E0%B8%AA%E0%B8%B1%E0%B8%87%E0%B8%AB%E0%B8%B2%E0%B8%A3-%E0%B9%80%E0%B8%94%E0%B8%AD%E0%B8%B0-%E0%B8%A1%E0%B8%B9%E0%B8%9F%E0%B8%A7%E0%B8%B5%E0%B9%88-%E0%B8%AB%E0%B9%89%E0%B8%A7%E0%B8%87%E0%B9%80%E0%B8%A7%E0%B8%A5%E0%B8%B2%E0%B8%82%E0%B8%AD%E0%B8%87%E0%B8%9E%E0%B8%A7%E0%B8%81%E0%B9%80%E0%B8%A3%E0%B8%B2.jpg',
+    url: 'https://enjoy24cdn.com/hlsr2/c241c118420386b8bd6905a4bd99a906/master.m3u8'
+  },
        {
     name: ' โอเวอร์ ลอร์ด 2',
     group: 'Anime',
